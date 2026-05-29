c961950ce17eda78ab16b2aff2d906737d4f77c1
Zeshan <zeshan@gmail.com>
2026-05-29 11:42:42 +0500

    Add complete calculator HTML

--- app.py
+++ app.py
@@ -4,7 +4,52 @@ app = Flask(__name__)
 
 HTML = """
 <!DOCTYPE html>
-...
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Calculator</title>
+</head>
+<body>
+<h1>ZESHAN'S CALCULATOR</h1>
+<input type="text" id="display" value="0" readonly style="font-size:24px;width:200px;text-align:right"><br><br>
+<button onclick="appendNum('7')">7</button>
+<button onclick="appendNum('8')">8</button>
+<button onclick="appendNum('9')">9</button>
+<button onclick="appendOp('+')">+</button><br>
+<button onclick="appendNum('4')">4</button>
+<button onclick="appendNum('5')">5</button>
+<button onclick="appendNum('6')">6</button>
+<button onclick="appendOp('-')">-</button><br>
+<button onclick="appendNum('1')">1</button>
+<button onclick="appendNum('2')">2</button>
+<button onclick="appendNum('3')">3</button>
+<button onclick="appendOp('*')">*</button><br>
+<button onclick="appendNum('0')">0</button>
+<button onclick="calculate()">=</button>
+<button onclick="clearAll()">AC</button>
+<button onclick="appendOp('/')">/</button>
+
+<script>
+let expr = '';
+function appendNum(n) { 
+  expr += n; 
+  document.getElementById('display').value = expr; 
+}
+function appendOp(op) { 
+  expr += op; 
+  document.getElementById('display').value = expr; 
+}
+function calculate() { 
+  document.getElementById('display').value = eval(expr); 
+  expr = document.getElementById('display').value; 
+}
+function clearAll() { 
+  expr = ''; 
+  document.getElementById('display').value = '0'; 
+}
+</script>
+</body>
+</html>
 """
 
 @app.route('/')
